2295b288a21e7950d8a12577fb25cac3b1091b56
hellochaeyoung <cyahn0831@hansung.ac.kr>
2022-09-08 21:01:32 +0900

    FEAN-55 MemberService 인터페이스 구현 : 회원정보 수정, 셀러 권한 수정, 셀러 조회

---
+++ src/main/java/com/lotte/danuri/member/members/MemberService.java
@@ -0,0 +1,17 @@
+package com.lotte.danuri.member.members;
+
+import com.lotte.danuri.member.members.dto.MemberInfoReqDto;
+import com.lotte.danuri.member.members.dto.SellerAuthReqDto;
+import java.util.List;
+import java.util.Optional;
+
+public interface MemberService {
+
+    long updateMemberInfo(MemberInfoReqDto dto);
+
+    int updateSellerAuth(SellerAuthReqDto dto);
+
+    Optional<List<Member>> getSellers(SellerAuthReqDto dto);
+
+
+}
