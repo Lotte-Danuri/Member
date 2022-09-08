6ee28271b437dce9a0ef8647a6872f3c43c6cf75
hellochaeyoung <cyahn0831@hansung.ac.kr>
2022-09-08 20:57:59 +0900

    FEAN-55 엔티티 공통 필드 가진 BaseEntity 생성

---
+++ src/main/java/com/lotte/danuri/member/domain/BaseEntity.java
@@ -0,0 +1,34 @@
+package com.lotte.danuri.member.domain;
+
+import java.time.LocalDateTime;
+import javax.persistence.Column;
+import javax.persistence.EntityListeners;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.MappedSuperclass;
+import lombok.Getter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+@EntityListeners(AuditingEntityListener.class)
+@Getter
+@MappedSuperclass
+public abstract class BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @CreatedDate
+    @Column(updatable = false)
+    private LocalDateTime createdDate;
+    private long createdBy;
+
+    @LastModifiedDate
+    private LocalDateTime updatedDate;
+    private long updatedBy;
+
+    private LocalDateTime deletedDate;
+}
