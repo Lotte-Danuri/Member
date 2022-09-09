cddcc94d3837032dd16ff46ebacc8bd2c2539395
hellochaeyoung <cyahn0831@hansung.ac.kr>
2022-09-09 16:23:48 +0900

    FEAN-30 AdminController 구현 - Seller 수정(등록,삭제),조회 API 구현

---
+++ src/main/java/com/lotte/danuri/member/members/AdminController.java
@@ -0,0 +1,32 @@
+package com.lotte.danuri.member.members;
+
+import com.lotte.danuri.member.members.dto.SellerAuthReqDto;
+import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/admin")
+@AllArgsConstructor
+public class AdminController {
+
+    private final AdminServiceImpl adminService;
+
+    @PatchMapping("/sellers")
+    public ResponseEntity<?> manageSellers(@RequestBody SellerAuthReqDto dto) {
+        long updatedMemberId = adminService.updateSellerAuth(dto);
+
+        return new ResponseEntity<>(updatedMemberId, HttpStatus.OK);
+    }
+
+    @GetMapping("/sellers")
+    public ResponseEntity<?> getSellers(@RequestBody SellerAuthReqDto dto) {
+        return new ResponseEntity<>(adminService.getSellers(dto), HttpStatus.OK);
+    }
+
+}
