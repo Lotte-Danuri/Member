09a936e945d47854fedcc39c7072af5232cbb7bf
hellochaeyoung <cyahn0831@hansung.ac.kr>
2022-09-12 20:25:40 +0900

    FEAN-63 Store Request, Response DTO 구현

---
+++ src/main/java/com/lotte/danuri/member/store/dto/StoreDto.java
@@ -0,0 +1,47 @@
+package com.lotte.danuri.member.store.dto;
+
+import com.lotte.danuri.member.members.Member;
+import com.lotte.danuri.member.store.Store;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@AllArgsConstructor
+@Builder
+public class StoreDto {
+
+    private long id;
+
+    private long sellerId;
+
+    private String name;
+
+    private String description;
+
+    private String address;
+
+    private String ownerName;
+
+    private String ownerNumber;
+
+    private String image;
+
+    private double score;
+
+    public Store toEntity(Member member) {
+        return Store.builder()
+            .member(member)
+            .name(name)
+            .descrption(description)
+            .address(address)
+            .score(score)
+            .ownerName(ownerName)
+            .ownerNumber(ownerNumber)
+            .image(image)
+            .build();
+    }
+
+}
