f0c236aa49de7d5dd4d0211f1e3712e8554f638c
hellochaeyoung <cyahn0831@hansung.ac.kr>
2022-09-16 15:16:03 +0900

    FEAN-55 in절을 이용한 스토어 조회 기능 구현

--- src/main/java/com/lotte/danuri/member/store/StoreRepository.java
+++ src/main/java/com/lotte/danuri/member/store/StoreRepository.java
@@ -1,11 +1,17 @@
 package com.lotte.danuri.member.store;
 
+import java.util.List;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface StoreRepository extends JpaRepository<Store, Long> {
 
-    Optional<Store> findByMemberId(long memberId);
+    Optional<Store> findByMemberId(Long memberId);
 
     Optional<Store> findByName(String name);
+
+    Optional<List<Store>> findByIdIn(List<Long> storeIds);
+
 }
