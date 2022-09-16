ccbd8c9b8314bbf0c2900f43daf9f7ba0df8e1eb
hellochaeyoung <cyahn0831@hansung.ac.kr>
2022-09-16 15:15:01 +0900

    FEAN-55 Store - Follow 일대다 양방향 연관관계 설정

--- src/main/java/com/lotte/danuri/member/store/Store.java
+++ src/main/java/com/lotte/danuri/member/store/Store.java
@@ -1,11 +1,16 @@
 package com.lotte.danuri.member.store;
 
 import com.lotte.danuri.member.domain.BaseEntity;
+import com.lotte.danuri.member.follow.Follow;
 import com.lotte.danuri.member.members.Member;
 import com.lotte.danuri.member.store.dto.StoreDto;
 import java.time.LocalDateTime;
+import java.util.List;
+import javax.persistence.CascadeType;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.JoinColumn;
+import javax.persistence.OneToMany;
 import javax.persistence.OneToOne;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -26,11 +31,11 @@ public class Store extends BaseEntity {
 
     private String name;
 
-    private String descrption;
+    private String description;
 
     private String address;
 
-    private double score;
+    private Double score;
 
     private String ownerName;
 
@@ -40,13 +45,16 @@ public class Store extends BaseEntity {
 
     private LocalDateTime deletedDate;
 
+    @OneToMany(mappedBy = "store", fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+    private List<Follow> followList;
+
     public StoreDto toDto() {
         return StoreDto.builder()
             .id(getId())
             .sellerId(member.getId())
             .name(name)
             .address(address)
-            .description(descrption)
+            .description(description)
             .ownerName(ownerName)
             .ownerNumber(ownerNumber)
             .image(image)
@@ -56,7 +64,7 @@ public class Store extends BaseEntity {
 
     public void update(StoreDto dto) {
         this.name = dto.getName();
-        this.descrption = dto.getDescription();
+        this.description = dto.getDescription();
         this.address = dto.getAddress();
         this.ownerName = dto.getOwnerName();
         this.ownerNumber = dto.getOwnerNumber();
