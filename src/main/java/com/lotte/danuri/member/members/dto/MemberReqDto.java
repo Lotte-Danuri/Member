382cf9318529bb04363032748645271272ef4bb8
hellochaeyoung <cyahn0831@hansung.ac.kr>
2022-09-16 15:09:31 +0900

    FEAN-55 스토어 팔로우한 회원 목록 조회를 위한 Member Request, Response DTO 구현

---
+++ src/main/java/com/lotte/danuri/member/members/dto/MemberReqDto.java
@@ -0,0 +1,16 @@
+package com.lotte.danuri.member.members.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class MemberReqDto {
+
+    private Long memberId;
+
+}
---
+++ src/main/java/com/lotte/danuri/member/members/dto/MemberRespDto.java
@@ -0,0 +1,24 @@
+package com.lotte.danuri.member.members.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+@Builder
+@AllArgsConstructor
+public class MemberRespDto {
+
+    private Long id;
+
+    private String name;
+
+    private String phoneNumber;
+
+    private String address;
+
+    private int age;
+
+    private int role;
+
+}
