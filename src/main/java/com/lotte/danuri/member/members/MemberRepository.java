c620aea5f5bb4793ae724e10f79513f8bfb93747
hellochaeyoung <cyahn0831@hansung.ac.kr>
2022-09-20 14:21:01 +0900

    FEAN-31 Member-Follow, Store-Follow 지연 로딩 적용과 삭제된 데이터(deletedDate) 제외하여 조회하기 위해 패치 조인 사용하여 적용

--- src/main/java/com/lotte/danuri/member/members/MemberRepository.java
+++ src/main/java/com/lotte/danuri/member/members/MemberRepository.java
@@ -2,10 +2,22 @@ package com.lotte.danuri.member.members;
 
 import java.util.List;
 import java.util.Optional;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface MemberRepository extends JpaRepository<Member, Long> {
 
     // [관리자] 판매자 조회 - role:3, status: -1(탈퇴),0(대기중),1(권한부여완료)
     Optional<List<Member>> findByRoleAndStatus(int role, int status);
+
+    @Query(
+        value = "SELECT distinct m FROM member m JOIN FETCH m.followList f "
+            + "WHERE m.id = :memberId "
+            + "AND f.member.id = :memberId "
+            + "AND m.deletedDate is null "
+            + "AND f.deletedDate is null "
+    )
+    Optional<Member> findById(Long memberId);
 }
