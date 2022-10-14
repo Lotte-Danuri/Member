f8aaa3d23587c39a008a82a9241c238da3b40c5c
hellochaeyoung <cyahn0831@hansung.ac.kr>
2022-10-14 18:17:32 +0900

    FEAN-156 @CrossOrigin 추가

--- src/main/java/com/lotte/danuri/member/MemberApplication.java
+++ src/main/java/com/lotte/danuri/member/MemberApplication.java
@@ -4,10 +4,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 @SpringBootApplication
 @EnableDiscoveryClient
 @EnableJpaAuditing
+@CrossOrigin("*")
 public class MemberApplication {
 
 	public static void main(String[] args) {
