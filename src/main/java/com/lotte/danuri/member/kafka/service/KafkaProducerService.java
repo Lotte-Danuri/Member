23b6f27ee64df36ed6f16251ca89afb97dfb90e0
hellochaeyoung <cyahn0831@hansung.ac.kr>
2022-10-18 15:38:38 +0900

    FEAN-168 [feat] Kafka Producer 구현 - 상품 좋아요 카운트 업데이트

---
+++ src/main/java/com/lotte/danuri/member/kafka/service/KafkaProducerService.java
@@ -0,0 +1,8 @@
+package com.lotte.danuri.member.kafka.service;
+
+import com.lotte.danuri.member.likes.dto.LikesInsertReqDto;
+import com.lotte.danuri.member.likes.dto.LikesReqDto;
+
+public interface KafkaProducerService {
+    LikesReqDto send(String topic, LikesReqDto couponDto);
+}
