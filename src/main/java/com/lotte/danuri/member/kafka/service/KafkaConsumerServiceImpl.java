502c9108e9cfafc707d18f921451323068169b13
jinwooooooo <ko440338@gmail.com>
2022-11-01 20:11:43 +0900

    (dev) fix : @Transactional 추가

--- src/main/java/com/lotte/danuri/member/kafka/service/KafkaConsumerServiceImpl.java
+++ src/main/java/com/lotte/danuri/member/kafka/service/KafkaConsumerServiceImpl.java
@@ -21,10 +21,12 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @Slf4j
 @RequiredArgsConstructor
+@Transactional
 public class KafkaConsumerServiceImpl implements  KafkaConsumerService{
 
     private final MyCouponRepository myCouponRepository;
