874af2c8cec29a8c0310a0eaa6738c12865c1bf9
hellochaeyoung <cyahn0831@hansung.ac.kr>
2022-11-03 09:26:04 +0900

    FEAN-225 [feat] @RequestBody String -> DTO로 수정

--- src/main/java/com/lotte/danuri/member/likes/LikesController.java
+++ src/main/java/com/lotte/danuri/member/likes/LikesController.java
@@ -1,5 +1,6 @@
 package com.lotte.danuri.member.likes;
 
+import com.lotte.danuri.member.likes.dto.LikesReqDto;
 import io.swagger.annotations.ApiOperation;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -23,23 +24,23 @@ public class LikesController {
     @PostMapping()
     @ApiOperation(value = "좋아요 등록", notes = "상품 좋아요 등록(추가)")
     public ResponseEntity<?> register(@RequestHeader String memberId,
-                                        @RequestBody String productCode) {
-        return new ResponseEntity<>(likesService.register(Long.parseLong(memberId), productCode), HttpStatus.CREATED);
+                                        @RequestBody LikesReqDto dto) {
+        return new ResponseEntity<>(likesService.register(Long.parseLong(memberId), dto.getProductCode()), HttpStatus.CREATED);
     }
 
     @PostMapping("/check")
     @ApiOperation(value = "좋아요 체크", notes = "좋아요한 상품인지 확인 조회")
     public ResponseEntity<?> check(@RequestHeader String memberId,
-                                    @RequestBody String productCode) {
-        boolean result = likesService.checkLike(Long.parseLong(memberId), productCode);
+                                    @RequestBody LikesReqDto dto) {
+        boolean result = likesService.checkLike(Long.parseLong(memberId), dto.getProductCode());
         return new ResponseEntity<>(result, HttpStatus.OK);
     }
 
     @DeleteMapping()
     @ApiOperation(value = "좋아요 취소", notes = "상품 좋아요 삭제(취소)")
     public ResponseEntity<?> delete(@RequestHeader String memberId,
-                                    @RequestBody String productCode) {
-        int result = likesService.delete(Long.parseLong(memberId), productCode);
+                                    @RequestBody LikesReqDto dto) {
+        int result = likesService.delete(Long.parseLong(memberId), dto.getProductCode());
         return new ResponseEntity<>(result, HttpStatus.OK);
     }
 }
